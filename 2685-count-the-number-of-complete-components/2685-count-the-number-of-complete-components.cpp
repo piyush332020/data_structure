87624b4ff0d6f0b5a7b0cd3514945145ab1f7c12
piyush kumar singh <piyushsingh7984@gmail.com>
2026-07-11 16:00:48 +0530

    Time: 67 ms (30.27%), Space: 137.4 MB (26.91%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,42 +1,39 @@
 class Solution {
 private:
-    void dfs(int node,vector<int>& visited,vector<int>& component,vector<vector<int>>& adj){
-        visited[node]=1;
+    void dfs(int node, vector<int>& visited, vector<int>& component,vector<vector<int>>& adj){
+        visited[node] = 1;
         component.push_back(node);
-        for(auto num : adj[node]){
-            if(!visited[num]){
-                dfs(num,visited,component,adj);
+        for (auto num : adj[node]) {
+            if (!visited[num]) {
+                dfs(num, visited, component, adj);
             }
         }
     }
 public:
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>>adj(n);
-        for(auto e : edges){
-            int u=e[0];
-            int v=e[1];
+    int countCompleteComponents(int n, vector<vector<int>>& edges){
+        vector<vector<int>> adj(n);
+        for (auto e : edges) {
+            int u = e[0];
+            int v = e[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        vector<int>visited(n,0);
-        vector<vector<int>>components;
-        for(int i=0;i<n;i++){
-            vector<int>component;
-            if(!visited[i]){
-                dfs(i,visited,component,adj);
-                components.push_back(component);
-            }
-        }
-        int count=0;
-        for (auto component : components) {
-            int vertices = component.size();
-            int edges = 0;
-            for (auto node : component) {
-                edges += adj[node].size();
-            }
-            edges /= 2;
-            if (edges == vertices * (vertices - 1) / 2) {
-                count++;
+        vector<int> visited(n, 0);
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                vector<int> component;
+                dfs(i, visited, component, adj);
+                int vertices = component.size();
+                int edgeCount = 0;
+                for (auto node : component) {
+                    edgeCount += adj[node].size();
+                }
+                edgeCount /= 2;
+
+                if (edgeCount == vertices * (vertices - 1) / 2) {
+                    count++;
+                }
             }
         }
         return count;
