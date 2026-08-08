de2c5961dd4a6882333f6b166ca4ee0794d9c927
piyush kumar singh <piyushsingh7984@gmail.com>
2026-08-08 18:04:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        vector<int>ans;
+        if (nums.size()<=1) return;
+        for(int i=nums.size()-k;i<nums.size();i++){
+            ans.push_back(nums[i]);
+        }
+        for(int i=0;i<nums.size()-k;i++){
+            ans.push_back(nums[i]);
+        }
+        nums=ans;
+    }
+};
