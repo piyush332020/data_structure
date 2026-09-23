425ee0354594f22773e71deb3e4e428b0ebed755
piyush kumar singh <piyushsingh7984@gmail.com>
2026-09-23 17:17:37 +0530

    Time: 3 ms (71.49%), Space: 102.4 MB (65.24%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int totalSum = 0;
-        for (int num : nums) {
-            totalSum += num;
+        int total=0;
+        for(int i=0;i<nums.size();i++){
+            total+=nums[i];
         }
-        int target = totalSum - x;
-
-        if (target < 0) {
-            return -1;
-        }
-        if (target == 0) {
-            return nums.size();
-        }
-        int left = 0;
-        int currSum = 0;
-        int maxLength = -1;
-        for (int right = 0; right < nums.size(); right++) {
-            currSum += nums[right];
-            while (currSum > target && left <= right) {
-                currSum -= nums[left];
+        int target=total-x;
+        if(target==0) return nums.size();
+        if(target<0) return -1;
+        int left=0;
+        int currlen=-1;
+        int sum=0;
+        for(int right=0;right<nums.size();right++){
+            sum+=nums[right];
+            while(sum>target && left<=right){
+                sum-=nums[left];
                 left++;
             }
-            if (currSum == target) {
-                maxLength = max(maxLength, right - left + 1);
+            if(sum==target){
+                currlen=max(currlen,right-left+1);
             }
         }
-        if (maxLength == -1) {
+        if(currlen==-1){
             return -1;
         }
-        return nums.size() - maxLength;
+        return nums.size()-currlen;
     }
 };
